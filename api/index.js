dccc2b83f80aa51bbafe25b21337b62eacff1d67
rifqiahmadabiyyi <rifqiahmad.abi@gmail.com>
2024-05-28 21:46:36 +0700

    update vercel.json

--- api/index.js
+++ api/index.js
@@ -2,10 +2,11 @@ import express from "express";
 import cors from "cors";
 import db from "../models/index.js";
 import productRouter from "../routes/productRoute.js";
+import setupSwagger from "../swaggerConfig.js";
 import dotenv from "dotenv";
 
 // Load environment variables
-dotenv.config();
+// dotenv.config();
 
 // App config
 const app = express();
@@ -26,6 +27,7 @@ db.sequelize.sync()
 app.use("/api/products", productRouter);
 app.use("/images", express.static('uploads'));
 
+setupSwagger(app);
 app.get("/", (req, res) => {
     res.send("Welcome to Grocery Store API");
 });
