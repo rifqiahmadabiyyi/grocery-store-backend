5448a007a66ee366bd7e043ec84b1b90f0eb21b9
rifqiahmadabiyyi <rifqiahmad.abi@gmail.com>
2024-05-28 21:25:17 +0700

    fixing deploy vercel

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 import express from "express"
 import cors from "cors"
-import db from "./models/index.js";
-import productRouter from "./routes/productRoute.js";
-import setupSwagger from "./swaggerConfig.js";
+import db from "../models/index.js";
+import productRouter from "../routes/productRoute.js";
+import setupSwagger from "../swaggerConfig.js";
 
 
 // app config
