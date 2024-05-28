bcbc5678596c0bd4d470bdecb1066741e583ec6f
rifqiahmadabiyyi <rifqiahmad.abi@gmail.com>
2024-05-28 21:12:44 +0700

    update api/index.js

--- api/index.js
+++ api/index.js
@@ -2,12 +2,16 @@ import express from "express";
 import cors from "cors";
 import db from "../models/index.js";
 import productRouter from "../routes/productRoute.js";
+import dotenv from "dotenv";
 
-// app config
+// Load environment variables
+dotenv.config();
+
+// App config
 const app = express();
-const port = 4000; // This is not used in serverless environment but kept for local dev
+const port = 4000; // Not used in serverless environment but kept for local dev
 
-// middleware
+// Middleware
 app.use(express.json());
 app.use(cors());
 
@@ -26,5 +30,5 @@ app.get("/", (req, res) => {
     res.send("Welcome to Grocery Store API");
 });
 
-// Export the app as a module to be used in serverless function
+// Export the app as a module
 export default app;
